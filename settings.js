b979fbe0f3ad5f16f294224194aff25f941713bc
alpinnio <107537249+alpinnio@users.noreply.github.com>
2022-07-14 23:28:32 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,18 +19,18 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['Your Number']
-global.premium = ['Your Number']
-global.ownernomer = 'Your Number'
-global.ownername = '🐦Star Boy Nexus'
-global.botname = 'Gojo-Satoru'
-global.footer = '©NexusNwInc.'
+global.owner = ['6288210072756']
+global.premium = ['6288210072756']
+global.ownernomer = '6288210072756'
+global.ownername = 'LuxxyXyz'
+global.botname = 'LuccyBot'
+global.footer = '©LuxxyNwInc.'
 global.ig = 'https://github.com/nexusNw'
 global.region = 'India, South India, Kerala'
 global.sc = 'https://github.com/nexusNw/Gojo-Satoru'
 global.myweb = 'https://youtube.com/channel/UCqoUjPvDdb0kjXNYdvPPpHQ'
-global.packname = 'Gojo-Satoru'
-global.author = '🐦Star Boy Nexus'
+global.packname = 'LuccyBot'
+global.author = 'LuxxyXyz'
 global.sessionName = 'session'
 global.prefa = ['','!','.','🐦','🐤','🗿']
 global.sp = '⭔'
